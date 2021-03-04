4d50fd4e375f80be68b223ccf5324b942b55089a
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-03-04 22:43:51 +0530

    Added APIs

--- Backend/app.py
+++ Backend/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, request
+from flask import Flask
 import requests
 
 app = Flask(__name__)
-key = input()
-curl = "https://graphhopper.com/api/1/geocode?q=Vellore&locale=en&debug=true&key={key}" # Destination text -> coordinates API
+key1 = str(input("Enter API Key: "))
+curl = f"https://graphhopper.com/api/1/geocode?q=Vellore&locale=en&debug=true&key={key1}" # Destination text -> coordinates API
 r = requests.get(curl.format()).json()
 @app.route('/')
 def home():
     return(r)
 
-url = "https://graphhopper.com/api/1/matrix?point=12.971774,79.161562&point=12.973510,79.168793&type=json&vehicle=car&debug=true&out_array=weights&out_array=times&out_array=distances&key={key}"  # Distance API
+url = f"https://graphhopper.com/api/1/matrix?point=12.971774,79.161562&point=12.973510,79.168793&type=json&vehicle=car&debug=true&out_array=weights&out_array=times&out_array=distances&key={key1}"  # Distance API
 r = requests.get(url.format()).json()
 
 if "__main__" == __name__:
