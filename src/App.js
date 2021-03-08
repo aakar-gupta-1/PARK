14a01c6411cfa8ef586a1ed02a1a77c2eb373ea9
parthsharma1410 <parth.sharma2019a@vitstudent.ac.in>
2021-03-08 23:22:41 +0530

    Added basic frontend code, made using reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Home, Home2, Home3} from "./Components";
 import './App.css';
+import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+        </Switch>
+        <Switch>
+          <Route path="/home2" exact component={() => <Home2 />} />
+        </Switch>
+        <Switch>
+          <Route path="/home3" exact component={() => <Home3 />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
